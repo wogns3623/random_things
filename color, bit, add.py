a10520ecc3a111e8ecd47e5303985bd50cbd8a33
wogns3623 <wogns3623@gmail.com>
2019-12-25 11:56:42 +0900

    Practice bitwise operation, divide color space, adding two image

---
+++ color, bit, add.py
@@ -0,0 +1,98 @@
+import cv2
+import numpy as np
+
+
+def onMouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        print(x, y)
+
+# read Image
+image = cv2.imread("./image/babaisyou.jpg", cv2.IMREAD_ANYCOLOR)
+
+cv2.imshow("baba", image)
+cv2.setMouseCallback("baba", onMouse)
+
+# BGR         |	Blue, Green, Red 채널           | -
+# BGRA        | Blue, Green, Red, Alpha 채널    | -
+# RGB         |	Red, Green, Blue 채널           | -
+# RGBA        | Red, Green, Blue, Alpha 채널    | -
+# GRAY        | 단일 채널                       | 그레이스케일
+# BGR565      | Blue, Green, Red 채널           | 16 비트 이미지
+# XYZ         | X, Y, Z 채널                    | CIE 1931 색 공간
+# YCrCb       |	Y, Cr, Cb 채널                  | YCC (크로마)
+# HSV         |	Hue, Saturation, Value 채널     | 색상, 채도, 명도
+# Lab         |	L, a, b 채널                    | 반사율, 색도1, 색도2
+# Luv         |	L, u, v 채널                    | CIE Luv
+# HLS         |	Hue, Lightness, Saturation 채널 |	색상, 밝기, 채도
+# YUV         |	Y, U, V 채널                    | 밝기, 색상1, 색상2
+# BG, GB, RG  | 디모자이킹                      |	단일 색상 공간으로 변경
+# _EA         | 디모자이킹                      | 가장자리 인식
+# _VNG        | 디모자이킹	                    | 그라데이션 사용
+grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("grey Baba", grey)
+
+image_r = image[:, :, 2]
+image_g = image[:, :, 1]
+image_b = image[:, :, 0]
+cv2.imshow("r Baba", image_r)
+cv2.imshow("g Baba", image_g)
+cv2.imshow("b Baba", image_b)
+
+image_copy = image.copy()
+image_copy[:, :, :2] = 0
+cv2.imshow("baba is red", image_copy)
+
+
+not1 = cv2.bitwise_not(image)
+cv2.imshow("not Baba", not1)
+or1 = cv2.bitwise_or(image, cv2.flip(image, 1))
+cv2.imshow("abaB and Baba", or1)
+binRet, bin1 = cv2.threshold(grey, 100, 225, cv2.THRESH_BINARY)
+cv2.imshow("01 Baba", bin1)
+
+keke = image[149:222, 17:74]
+cv2.imshow("keke", keke)
+
+src = cv2.imread("./image/test.jpg", cv2.IMREAD_ANYCOLOR)
+
+kekebg = np.zeros(src.shape, np.uint8)
+lh = (src.shape[0]-keke.shape[0])//2
+lw = (src.shape[1]-keke.shape[1])//2
+kekebg[lh:lh+keke.shape[0],lw:lw+keke.shape[1]] = keke
+
+src_c = src.copy()
+src_c[lh:lh+keke.shape[0],lw:lw+keke.shape[1]] = keke
+
+cv2.imshow("add0", src_c)
+cv2.imshow("add1", cv2.bitwise_or(src, kekebg))
+cv2.imshow("add2", cv2.add(src, kekebg))
+cv2.imshow("add3", src+kekebg)
+
+# get mask, keke with black bg
+keke_grey = cv2.cvtColor(keke, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(keke_grey, 20, 255, cv2.THRESH_BINARY)
+mask_inv = cv2.bitwise_not(mask)
+
+bg = src[lh:lh+keke.shape[0],lw:lw+keke.shape[1]]
+bg_mask = cv2.bitwise_and(bg, bg, mask=mask_inv)
+keke_black_bg = cv2.bitwise_and(keke, keke, mask=mask)
+keke_with_bg = cv2.add(bg_mask, keke_black_bg)
+
+# cv2.imshow("1", keke)
+# cv2.imshow("2", bg)
+# cv2.imshow("3", keke_grey)
+# cv2.imshow("4", mask)
+# cv2.imshow("5", mask_inv)
+# cv2.imshow("6", bg_mask)
+# cv2.imshow("7", keke_black_bg)
+# cv2.imshow("8", keke_with_bg)
+keke_bg = src.copy()
+keke_bg[lh:lh+keke.shape[0],lw:lw+keke.shape[1]] = keke_with_bg
+cv2.imshow("result", keke_bg)
+
+
+
+# wait until any key input
+cv2.waitKey(0)
+# if read key input, destroy all windows
+cv2.destroyAllWindows()
